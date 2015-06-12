16477a64dd78c2da831e51bb0c5d8b424434cb20
xavierrocks <xruizify@gmail.com>
2015-06-11 21:22:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-function bindEvent(e, eventName, callback) {
-    if(e.addEventListener) // new browsers
-        e.addEventListener(eventName, callback, false);
-    else if(e.attachEvent) // IE
-        e.attachEvent('on'+ eventName, callback);
-};
 
-bindEvent(document.body, 'scroll', function(e) {
-    document.body.scrollLeft = 0;
-});
