5e42026fd88a53f37d798cb9ba409e79c9396e7c
Xavier Ruiz <xruizify@gmail.com>
2019-05-04 12:42:53 -0400

    added back some functions, edited json, moved folders

--- base.js
+++ base.js
@@ -23,25 +23,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-// function httpGetAsync(theUrl, callback) {
-//     var xmlHttp = new XMLHttpRequest();
-//     xmlHttp.onreadystatechange = function () {
-//         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-//             callback(xmlHttp.responseText);
-//     }
-//     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-//     xmlHttp.send(null);
-// }
+function httpGetAsync(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
 
 
 
 
-// function httpGetSync(theUrl) {
-//     var xmlHttp = new XMLHttpRequest();
-//     xmlHttp.open("GET", theUrl, false); // false for synchronous request
-//     xmlHttp.send(null);
-//     return xmlHttp.responseText;
-// }
+function httpGetSync(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
 
 
 // function renderItems(res) {
